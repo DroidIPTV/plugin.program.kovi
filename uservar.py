79d3296718e0585b158bfdaae348c2ecf24566bd
DroidIPTV <145887293+DroidIPTV@users.noreply.github.com>
2025-04-12 20:43:49 +0200

    Add files via upload

---
+++ uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://raw.githubusercontent.com/DroidIPTV/plugin.program.kovi/refs/heads/main/xml/wizard.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://raw.githubusercontent.com/DroidIPTV/plugin.program.kovi/refs/heads/main/xml/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.program.kovi']
